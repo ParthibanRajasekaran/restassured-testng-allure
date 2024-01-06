f9d8bb73edf951aec8af02f7678be82bc3ed9a5b
ParthibanRajasekaran <rajasekaran.parthiban7@gmail.com>
2024-01-06 11:58:46 +0000

    chore: adding product resource

--- src/main/java/com/template/project/product/resource/ProductResource.java
+++ src/main/java/com/template/project/product/resource/ProductResource.java
@@ -1,2 +1,6 @@
-package com.template.project.product.resource;public class ProductResource {
+package com.template.project.product.resource;
+
+public class ProductResource {
+
+    public static final String PRODUCT_RESOURCE = "/shop/v2/products/";
 }
