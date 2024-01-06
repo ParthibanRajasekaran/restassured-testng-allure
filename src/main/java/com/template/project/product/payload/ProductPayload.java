d8aeef6037de826a3966fd5b178e96f9a99dd28f
Anushka Parthiban <anushkaparthiban@Anushkas-MacBook-Pro.local>
2024-01-06 11:57:39 +0000

    chore: adding product payload

--- src/main/java/com/template/project/product/payload/ProductPayload.java
+++ src/main/java/com/template/project/product/payload/ProductPayload.java
@@ -1,2 +1,18 @@
-package com.template.project.product.payload;public class ProductPayload {
+package com.template.project.product.payload;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.template.project.api.builder.SampleBuilder;
+import com.template.project.api.model.SampleModel;
+import com.template.project.product.builder.ProductBuilder;
+import com.template.project.product.model.Product;
+
+import static com.template.project.api.builder.SampleBuilder.addCustomer;
+
+public class ProductPayload {
+
+    public static String buildProductCreationPayload(String productName, double productPrice)
+            throws JsonProcessingException {
+        Product product = ProductBuilder.addProduct(productName, productPrice);
+        return ProductBuilder.getJsonPayload(product);
+    }
 }
